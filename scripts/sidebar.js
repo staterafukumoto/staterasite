ccce839e0ccb922ff3d0e038dd8a7a973720ba84
wayoutware sp <48106340+wayoutware@users.noreply.github.com>
2020-04-06 05:40:56 -0500

    more basic site features
    now it uh... tells you where you are. probably should have had that from the start...

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -2,6 +2,18 @@ const content = document.getElementById("leftwrapper")
 
 async function load_home(){
     content.innerHTML = await (await fetch('/sidebar.html')).text();
+    assignMarker()
 }
 
+function assignMarker(){
+    var path = window.location.pathname
+    var origin = window.location.origin
+    var collection = document.getElementById("socialmedia").getElementsByTagName("A")
+
+    for (i = 0; i < collection.length; i++) {
+        if (collection.item(i).href.replace(origin,"") == path){
+            collection.item(i).classList.add("currentActive")
+        }
+    } 
+}
 document.body.onload = load_home
