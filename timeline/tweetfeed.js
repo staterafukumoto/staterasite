6de02976ca73dc9502840030ad79ca607f1a5267
statera fukumoto <stateralykos@gmail.com>
2020-07-23 01:39:25 -0500

    + new wallpapers
    yey

---
+++ timeline/tweetfeed.js
@@ -0,0 +1,11 @@
+// const divtweets = document.getElementById("TWITTERFEED")
+
+// function tweetThemeUpdate(){
+//     if(getTheme() == "dark"){
+//         divtweets.innerHTML = await ( fetch('darktweets.html')).text()
+//     } else{
+//         divtweets.innerHTML = await ( fetch('lighttweets.html')).text()
+//     }
+// }
+
+// tweetThemeUpdate()
