5edcad7f39142fda26bcd3f928485395ddc433c0
wayoutware sp <48106340+wayoutware@users.noreply.github.com>
2020-10-15 00:46:51 -0500

    more effecient math
    and changed versioning system

--- scripts/home_page_ONLY.js
+++ scripts/home_page_ONLY.js
@@ -1,43 +1,45 @@
+//halloween countdown code
 var lengthofday = 1000 * 60 * 60 * 24 
-var present_date = new Date(); 
+var present_date = new Date()
 var halloween = new Date(present_date.getFullYear(), 9, 31) 
-var math = Math.round(halloween.getTime() - present_date.getTime()) / (lengthofday); 
-var finalresult = math.toFixed(0); 
+var math = Math.round(halloween.getTime() - present_date.getTime()) / (lengthofday)
+var finalresult = math.toFixed(0)
 
-// console.log(Final_Result)
 document.getElementById("hlwncounter").innerHTML = finalresult
-
-function getScroll(){
-    var current = document.getElementById("rightwrapper").scrollTop
-    var total = document.getElementById('rightwrapper').scrollHeight / 2
-    var math = current / total
-    var moremath = math * 100
-    var moremoremath = Math.floor(moremath)
-}
-
-
-document.getElementById("rightwrapper").addEventListener("scroll", adjustBanner);
 
 function adjustBanner(){
     //higher the number, the slower it scrolls, and choppier it looks
-    // CANNOT BE HIGHER THAN 10
-    var scrollRatio = 4
+    // CANNOT BE GREATER THAN 10
+    var scrollRatio = 5.5
 
-    var current = document.getElementById("rightwrapper").scrollTop
-    var total = document.getElementById('rightwrapper').scrollHeight / 2
-    var math = current / total
-    var moremath = math * 100
-    var moremoremath = Math.floor(moremath)
+    var current = document.getElementById("rightwrapper").scrollTop         // current position
+    var total = document.getElementById('rightwrapper').scrollHeight / 2    // total page height, divide by 2 because it's faster than viewport math
+    var operation = current / total                                         // fractions!
+    var moremath = operation * 100                                          // now get it to a meaningful number
 
     if (mobilecheck() == true){
-        // do not scroll animate on phones
-    } else if(navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1){
-        // do not scroll animate on iPads :(
-    }else{
-        document.getElementById("mainsitebanner").style.top = moremoremath * scrollRatio + "px"
-    }
 
+        // do not scroll animate on phones, it breaks hard.
+
+    } else if(navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1){
+
+        // do not scroll animate on iPads, it breaks slightly less so, but still
+
+    }else{
+        if (moremath => 0){
+
+            document.getElementById("mainsitebanner").style.top = moremath * scrollRatio + "px"
+
+        } else{
+
+            //don't put the banner lower than the absolute top of the screen
+
+        }
+    }
 }
+
+document.getElementById("rightwrapper").addEventListener("scroll", adjustBanner);
+window.setTimeout(adjustBanner,8)
 
 
 
